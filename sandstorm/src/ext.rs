d4b9b869f7f909d0fe2db9d00dadcc36d2383a57
MichaelMorf <75577437+MichaelMorf@users.noreply.github.com>
2025-07-08 00:29:21 +0300

    Update ext.rs

--- sandstorm/src/ext.rs
+++ sandstorm/src/ext.rs
@@ -14,7 +14,6 @@
  */
 
 use hashbrown::HashMap;
-use std::ops::Generator;
 use std::pin::Pin;
 use std::rc::Rc;
 use std::sync::Arc;
@@ -30,7 +29,7 @@ use spin::RwLock;
 const EXT_BUCKETS: usize = 32;
 
 // The type signature of the function that will be searched for inside an so.
-type Proc = unsafe extern "C" fn(Rc<DB>) -> Pin<Box<Generator<Yield = u64, Return = u64>>>;
+type Proc = unsafe extern "C" fn(Rc<DB>) -> Pin<Box<dyn std::future::Future<Output = u64>>>;
 
 /// This type represents an extension that has been successfully loaded into
 /// the database. As long as this type is not dropped, the extension will exist
@@ -107,7 +106,7 @@ impl Extension {
     /// # Return
     ///
     /// A generator that can be scheduled by the database.
-    pub fn get(&self, db: Rc<DB>) -> Pin<Box<Generator<Yield = u64, Return = u64>>> {
+    pub fn get(&self, db: Rc<DB>) -> Pin<Box<dyn std::future::Future<Output = u64>>> {
         // Call into the procedure, and return the generator.
         unsafe { (self.procedure)(db) }
     }
@@ -132,6 +131,8 @@ impl ExtensionManager {
         ExtensionManager {
             // Can't use the copy constructor because of the Arc<Extension>.
             extensions: [
+                RwLock::new(HashMap::new()),
+                RwLock::new(HashMap::new()),
                 RwLock::new(HashMap::new()),
                 RwLock::new(HashMap::new()),
                 RwLock::new(HashMap::new()),
